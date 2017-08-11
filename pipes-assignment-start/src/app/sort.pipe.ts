a88d9a4c9f8306198e796574ad69f1d8ccf4a67c
unknown <Vikram.Singh@teamaol.com>
2017-08-11 17:21:01 +0530

    added pipe assignment

---
+++ pipes-assignment-start/src/app/sort.pipe.ts
@@ -0,0 +1,28 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'sort'
+})
+export class SortPipe implements PipeTransform {
+
+  transform(value: any, sortProp): any {
+    if(value.length === 0) {
+        return value;
+    }
+
+    return value.sort(this.dynamicSort(sortProp));
+  }
+
+  dynamicSort(property) {
+      var sortOrder = 1;
+      if(property[0] === "-") {
+          sortOrder = -1;
+          property = property.substr(1);
+      }
+      return function (a,b) {
+          var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+          return result * sortOrder;
+      }
+  }
+
+}
